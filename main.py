df51b360918740745267af22a13a2188be3742f5
SerDinadan <serdinadan@gmail.com>
2021-08-26 22:05:40 -0700

    Spirograph Complete

--- main.py
+++ main.py
@@ -5,22 +5,44 @@ tim = Turtle()
 tim.shape("turtle")
 
 
+def change_color():
+    screen.colormode(255)
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    tim.color(r, g, b)
+
+
 # Shape properties
 def draw_shapes(start_sides, end_sides):
     for sides in range(start_sides, end_sides + 1):
         number_of_sides = sides
         angle = 360 / number_of_sides
         # Randomize color
-        r = random.randint(1, 255)
-        g = random.randint(1, 255)
-        b = random.randint(1, 255)
-        tim.color(r, g, b)
+        change_color()
         for side in range(sides):
             tim.right(angle)
             tim.forward(100)
 
 
+def random_walk(number_of_times):
+    for i in range(number_of_times):
+        change_color()
+        angle = [90, 180, 270, 360]
+        tim.right(random.choice(angle))
+        tim.forward(15)
+
+
 screen = Screen()
-screen.colormode(255)
-draw_shapes(3, 10)
+tim.width(5)
+tim.speed(10)
+# random_walk(50)
+heading = 0
+for i in range(24):
+    change_color()
+    tim.setheading(heading)
+    tim.circle(70, 360)
+    heading += 15
+
+
 screen.exitonclick()
